bc1f8358eabf126e4ef6a08b43374abdaee9d6f4
keithgw <keith.gordon.williams@gmail.com>
2015-04-22 15:02:50 -0400

    added comparison features

--- server.R
+++ server.R
@@ -1,5 +1,6 @@
 library(shiny)
 library(ggplot2)
+library(dplyr)
 data(mtcars)
 
 # change transmission variable to factor and relabel
@@ -14,40 +15,67 @@ shinyServer(
     function(input, output) {
         
         # create prediction object with interval from inputs
-        mpg_hat <- reactive({
+        mpg_hat1 <- reactive({
             predict(fit, 
-                    data.frame(wt = input$weight / 1000,
-                               hp = input$horsepower,
-                               cyl = input$cylinders,
-                               am = input$transmission), 
+                    data.frame(wt = input$weight1 / 1000,
+                               hp = input$horsepower1,
+                               cyl = input$cylinders1,
+                               am = input$transmission1), 
+                    interval = "prediction")
+        })
+        mpg_hat2 <- reactive({
+            predict(fit, 
+                    data.frame(wt = input$weight2 / 1000,
+                               hp = input$horsepower2,
+                               cyl = input$cylinders2,
+                               am = input$transmission2), 
                     interval = "prediction")
         })
         
-        # return mpg prediction for printing
-        output$prediction <- renderText({
-            mpg_hat()[1]
+        # return mpg prediction 1 for printing
+        output$prediction1 <- renderText({
+            mpg_hat1()[1]
         })
         
-        # return prediction interval for printing
-        output$lower <- renderText({
-            mpg_hat()[2]
+        # return prediction interval 1 for printing
+        output$lower1 <- renderText({
+            mpg_hat1()[2]
         })
-        output$upper <- renderText({
-            mpg_hat()[3]
+        output$upper1 <- renderText({
+            mpg_hat1()[3]
+        })
+        
+        # return mpg prediction 2 for printing
+        output$prediction2 <- renderText({
+            mpg_hat2()[1]
+        })
+        
+        # return prediction interval 2 for printing
+        output$lower2 <- renderText({
+            mpg_hat2()[2]
+        })
+        output$upper2 <- renderText({
+            mpg_hat2()[3]
         })
         
         # create plot of prediction and interval
         output$mpg <- renderPlot({
-            ggplot(as.data.frame(mpg_hat())) +
-                geom_line(aes(c(1, 1), c(lwr, upr)), col="#F1BB7B") + 
-                geom_point(aes(1, fit), col="#5B1A18", size = 6) +
-                geom_point(aes(c(1, 1), c(lwr, upr)), 
+            ggplot(full_join(as.data.frame(mpg_hat1()),
+                             as.data.frame(mpg_hat2())
+                             )
+                   ) +
+                geom_line(aes(c(1, 1), c(lwr[1], upr[1])), col="#F1BB7B") + 
+                geom_line(aes(c(2, 2), c(lwr[2], upr[2])), col="#F1BB7B") +
+                geom_point(aes(1, fit[1]), col="#5B1A18", size = 6) +
+                geom_point(aes(2, fit[2]), col="#5B1A18", size = 6) +
+                geom_point(aes(c(1, 1), c(lwr[1], upr[1])), 
                            col="#D67236", size = 6, shape = 18) +
-                labs(x = "", y = "Miles per Gallon") +
-                theme(axis.title.y = element_text(size = 20),
-                      axis.text.x = element_blank()) + 
-                coord_cartesian(xlim = c(.9, 1.1),
-                                ylim = c(-2, 36))
+                geom_point(aes(c(2, 2), c(lwr[2], upr[2])), 
+                           col="#D67236", size = 6, shape = 18) +
+                labs(x = "Vehicle", y = "Miles per Gallon") +
+                theme(axis.title.y = element_text(size = 20)) + 
+                scale_x_discrete(limits = c(1, 2)) +
+                coord_cartesian(ylim = c(-2, 36))
         })
     }
 )
--- ui.R
+++ ui.R
@@ -5,48 +5,81 @@ shinyUI(
     pageWithSidebar(
         headerPanel("Fuel Economy Prediction"),
         sidebarPanel(
-            sliderInput('weight', 
+            h3("Vehicle 1"),
+            sliderInput('weight1', 
                         'Select weight (lb)', 
                         3325, 
                         min=1513,
                         max=5424,
                         step=10
                         ),
-            sliderInput('horsepower',
+            sliderInput('horsepower1',
                         'Select horsepower',
                         123.0,
                         min=52.0,
                         max=335.0,
                         step=1.0
                         ),
-            selectInput('cylinders',
+            selectInput('cylinders1',
                         'Number of cylinders',
                         c('4' = 4, '6' = 6, '8' = 8),
                         selected = '4'
                         ),
-            selectInput('transmission',
+            selectInput('transmission1',
                         'Transmission type',
                         c('automatic', 'manual')
+                        ),
+            h3("Vehicle 2"),
+            sliderInput('weight2', 
+                        'Select weight (lb)', 
+                        3325, 
+                        min=1513,
+                        max=5424,
+                        step=10
+                        ),
+            sliderInput('horsepower2',
+                        'Select horsepower',
+                        123.0,
+                        min=52.0,
+                        max=335.0,
+                        step=1.0
+                        ),
+            selectInput('cylinders2',
+                        'Number of cylinders',
+                        c('4' = 4, '6' = 6, '8' = 8),
+                        selected = '4'
+                        ),
+            selectInput('transmission2',
+                        'Transmission type',
+                        c('automatic', 'manual'),
+                        selected = 'manual'
                         )
             ),
         mainPanel(
             tabsetPanel(
                 tabPanel("Prediction",
-                         h3('Predicted Fuel Economy'),
+                         plotOutput("mpg", width = "40%"),
+                         h3('Predicted Fuel Economy: Vehicle 1'),
                          h4('Predicted miles per gallon'),
-                         verbatimTextOutput("prediction"),
+                         verbatimTextOutput("prediction1"),
                          h4('Prediction interval lower bound'),
-                         verbatimTextOutput("lower"),
+                         verbatimTextOutput("lower1"),
                          h4('Prediction interval upper bound'),
-                         verbatimTextOutput("upper"),
-                         plotOutput("mpg", width = "25%")
+                         verbatimTextOutput("upper1"),
+                         h3('Predicted Fuel Economy: Vehicle 2'),
+                         h4('Predicted miles per gallon'),
+                         verbatimTextOutput("prediction2"),
+                         h4('Prediction interval lower bound'),
+                         verbatimTextOutput("lower2"),
+                         h4('Prediction interval upper bound'),
+                         verbatimTextOutput("upper2")
                          ),
                 tabPanel("Documentation",
                          p(                           
-                             'This app is designed to give a predicted fuel
-                             economy (mpg) for a car with a given weight, 
-                             horsepower, number of cylinders, and 
-                             transmission type. The prediction is fit from 
+                             'This app is designed to compare predicted fuel
+                             economies (mpg) for two cars with given weights, 
+                             horsepower, numbers of cylinders, and 
+                             transmission types. The predictions are fit from 
                              a linear model based on the mtcars data set 
                              in R. This app is an extension of the project 
                              created in the regression models course.'
@@ -57,7 +90,8 @@ shinyUI(
                              parameters in the side bar. In addition to a 
                              prediction (in mpg), the user will also be 
                              given a prediction interval as a measure of 
-                             uncertainty.'
+                             uncertainty. The comparison is plotted for easy 
+                             visualization.'
                              )
                          )
                 )
